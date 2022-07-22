414a4b20e5ef6fb11d32a1523b3476d9d26168fa
JJ <84591407+JJBrindamour@users.noreply.github.com>
2022-07-21 20:51:11 -0400

    Update post-arch-install.sh

--- post-arch-install.sh
+++ post-arch-install.sh
@@ -1,21 +1,25 @@
-#!/usr/bin/bash
+!/usr/bin/bash
 
 echo "IMPORTANT: THIS SCRIPT IS STILL IN DEVELEOPEMENT, DO NOT USE ON REAL HARDWARE!"
-read -p " Do you wish to continue? (y/N): " cont
+read -p " Do you wish to continue? (y/N): " contin
 
-if [[ $cont == "y" || $cont == "Y"  ]]; then
+if [[ $contin == "y" || $contin == "Y"  ]]; then
 	sudo pacman -S xorg xorg-xinit picom firefox vim micro nano geany pcmanfm git alacritty bspwm sxhkd polybar htop neofetch nitrogen qt5ct dmenu lxappearance zsh breeze-gtk breeze-icons ttf-fira-code ttf-font-awesome networkmanager starship unzip exa base-devel
 
-	git clone https://aur.archlinux.org/yay.git
-	(cd yay && makepkg -si)
+	git clone https://aur.archlinux.org/yay.git $HOME/yay
+	(cd $HOME/yay && makepkg -si)
 
-	yay -S cava spotify ant-dracula-gtk-theme dracula-icons-gi ly-git arcolinux-logout nerd-fonts-fira-code
+	yay -S cava spotify ant-dracula-gtk-theme dracula-icons-git ly-git arcolinux-logout nerd-fonts-fira-code
 
-	git clone https://gitlab.com/dwt1/wallpapers ~/DTWallpapers
-	git clone https://github.com/JJBrindamour/Wallpapers ~/JJWallpapers
+	git clone https://gitlab.com/dwt1/wallpapers $HOME/DTWallpapers
+	git clone https://github.com/JJBrindamour/Wallpapers $HOME/JJWallpapers
 
-	rm -rf ~/.config
-	git clone https://github.com/JJBrindamour/Dotfiles ~/.config
+	rm -rf $HOME/.config
+	git clone https://github.com/JJBrindamour/Dotfiles $HOME/.config
+
+	mv $HOME/.config/.zshrc $HOME/
+	mv $HOME/.config/.bashrc $HOME/
+	mv $HOME/.config/.config $HOME/.config
 
 	nitrogen --set-scaled ~/JJWallpapers/0041.png
 
